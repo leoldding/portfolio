604dacf4599e1c5b154979680b822b29ef132035
leoldding <leo.ld.ding@gmail.com>
2023-08-30 13:24:53 -0400

    create featured projects section

--- frontend/src/components/portftolio/js/ProjectCard.js
+++ frontend/src/components/portftolio/js/ProjectCard.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 function ProjectCard(props) {
     return (
-        <div className={"py-2 md:w-1/3"}>
+        <div className={"py-2 " + (props.featured ? "md:w-1/2" : "md:w-1/3")}>
             <Link to={props.link}
                   target={"_blank"}
                   rel={"noopener noreferrer"}
-                  className={"block md:w-11/12 h-52 bg-orange-200 rounded-md transition duration-300 md:hover:bg-orange-400 md:hover:-translate-y-1 overflow-hidden"}>
-                <div className={"px-4"}>
-                    <h2 className={"text-lg md:text-2xl py-2"}>{props.title}</h2>
-                    <p className={"text-xs md:text-sm py-2"}>{props.description}</p>
+                  className={"block mx-auto w-[98%] h-52 bg-orange-200 rounded-md transition duration-300 md:hover:bg-orange-400 md:hover:-translate-y-1 overflow-hidden"}>
+                <div className={"px-6 py-4"}>
+                    <h2 className={"text-lg md:text-2xl pb-2"}>{props.title}</h2>
+                    <p className={"text-xs md:text-sm"}>{props.description}</p>
                 </div>
             </Link>
         </div>
--- frontend/src/components/portftolio/js/Projects.js
+++ frontend/src/components/portftolio/js/Projects.js
@@ -5,20 +5,33 @@ function Projects() {
     return (
         <div className={"max-w-screen-xl px-16 py-4 mx-auto"}>
             <h1 className={"text-2xl md:text-4xl pb-4"}>
-                Projects
+                Featured Projects
             </h1>
-            <div className={"flex flex-col md:flex-row flex-wrap"}>
+            <div className={"flex flex-col md:flex-row flex-wrap mb-4 md:mb-8"}>
                 <ProjectCard link={"https://github.com/leoldding"}
                              title={"Project 1"}
-                             description={"Description"} />
+                             description={"Description"}
+                             featured={true} />
                 <ProjectCard link={"https://github.com/leoldding"}
                              title={"Project 2"}
-                             description={"Description"} />
+                             description={"Description"}
+                             featured={true} />
+            </div>
+            <h1 className={"text-2xl md:text-4xl pb-4"}>
+                Other Projects
+            </h1>
+            <div className={"flex flex-col md:flex-row flex-wrap"}>
                 <ProjectCard link={"https://github.com/leoldding"}
                              title={"Project 3"}
                              description={"Description"} />
                 <ProjectCard link={"https://github.com/leoldding"}
                              title={"Project 4"}
+                             description={"Description"} />
+                <ProjectCard link={"https://github.com/leoldding"}
+                             title={"Project 5"}
+                             description={"Description"} />
+                <ProjectCard link={"https://github.com/leoldding"}
+                             title={"Project 6"}
                              description={"Description"} />
             </div>
         </div>
