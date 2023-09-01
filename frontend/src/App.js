227262e19c81f36160844252317a26cf0fe9cdb3
leoldding <leo.ld.ding@gmail.com>
2023-09-01 17:08:00 -0400

    added 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./components/portftolio/js/Home";
-import Projects from "./components/portftolio/js/Projects";
-import Navbar from "./components/portftolio/js/Navbar";
+import Home from "./components/portfolio/js/Home";
+import Projects from "./components/portfolio/js/Projects";
+import Navbar from "./components/portfolio/js/Navbar";
+import NotFound from "./components/portfolio/js/404";
 
 import "./styles.css";
 
 function App() {
     return (
-      <div>
+      <div className={"h-screen"}>
         <Router>
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/projects" element={<Projects/>}/>
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </Router>
       </div>
