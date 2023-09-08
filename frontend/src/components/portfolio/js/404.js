7fb0f099824d19b2d82a8046497ddf0800f5342f
leoldding <leo.ld.ding@gmail.com>
2023-09-08 12:28:30 -0400

    404 background and text color change

--- frontend/src/components/portfolio/js/404.js
+++ frontend/src/components/portfolio/js/404.js
@@ -4,19 +4,21 @@ import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDiss
 
 function NotFound() {
     return (
-        <div className={"w-full h-[calc(100%-110px)] bg-gradient-to-b from-orange-100 to-orange-400 flex flex-col items-center justify-center space-y-12"}>
-            <h1 className={"text-6xl md:text-8xl italic"}>
+        <div className={"w-full h-[calc(100%-110px)] flex flex-col items-center justify-center space-y-12"}>
+            <h1 className={"bg-gradient-to-br from-orange-100 to-orange-600 text-transparent bg-clip-text text-8xl md:text-9xl italic"}>
                 404
             </h1>
-            <p className={"text-lg md:text-2xl"}>
+            <p className={"text-lg md:text-2xl bg-gradient-to-br from-orange-100 to-orange-600 text-transparent bg-clip-text"}>
                 PAGE NOT FOUND
-                <SentimentVeryDissatisfiedIcon className={"pb-1"} />
+                <SentimentVeryDissatisfiedIcon className={"pb-1 text-orange-500"} />
             </p>
-            <button className={"p-4 rounded-md border-black border-2 transition duration-300 md:hover:bg-orange-400 md:hover:-translate-y-1"}>
-                <Link to={"/"} className={"text-sm md:text-lg"}>
-                    Go Home
-                </Link>
-            </button>
+            <Link to={"/"} className={"text-white text-md md:text-xl"}>
+                <div className={"bg-orange-200 rounded-md"}>
+                    <div className={"p-4 rounded-md bg-gradient-to-br from-orange-100 to-orange-600 transition duration-300 md:hover:-translate-y-1 md:hover:-translate-x-1"}>
+                            Go Home
+                    </div>
+                </div>
+            </Link>
         </div>
     );
 }
