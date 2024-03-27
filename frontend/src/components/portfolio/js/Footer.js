f2875cc2f3cdff66d539af962053ee0439b1dca5
leoldding <leo.ld.ding@gmail.com>
2024-03-27 13:56:49 -0400

    updated footer

--- frontend/src/components/portfolio/js/Footer.js
+++ frontend/src/components/portfolio/js/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
       <div className={"relative bottom-0 w-full h-[36px] bg-orange-200 flex flex-col justify-center items-center"}>
           <div className={"text-xs"}>
-             Leo Ding <Copyright sx={{fontSize: "14px"}}/> 2023. Hosted on AWS.
+             Leo Ding <Copyright sx={{fontSize: "14px"}}/> {(new Date().getFullYear())}. Hosted on AWS.
           </div>
       </div>
     );
